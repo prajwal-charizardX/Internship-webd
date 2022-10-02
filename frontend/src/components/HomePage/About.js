40cd4abd4171d3746d69ee4ac590f0a118acea6b
2deadmen <kartikhegde.2002@gmail.com>
2022-10-02 18:42:10 +0530

    kg

---
+++ frontend/src/components/HomePage/About.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './About.css'
+
+export class About extends Component {
+  render() {
+    return (
+     <div className='container-about'><div className='grid'>
+        <div  className='grid-ele'>
+           01</div>
+        <div className='grid-ele'>
+           02</div>
+      </div></div> 
+    )
+  }
+}
+
+export default About;
--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Cards from './Cards'
 import Header from './Header'
 import Footer from './Footer'
+import About from './About'
 
 function HomePage(props) {
   return (
     <>
     <Header/>
     <Cards setloader={props.setloader} />
+    <About></About>
     <Footer></Footer>
     </>
   )
