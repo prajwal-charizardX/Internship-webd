ff0d0dcd6761f85875baed15c6d18ec2d917c422
2deadmen <kartikhegde.2002@gmail.com>
2022-10-02 22:54:56 +0530

    expr

--- frontend/src/components/HomePage/Exp.js
+++ frontend/src/components/HomePage/Exp.js
@@ -1,12 +1,45 @@
-import React, { Component } from 'react'
-import './Exp.css'
+import React, { Component } from "react";
+import "./Exp.css";
 
 export class Exp extends Component {
   render() {
     return (
-      <div className='exp-pic-div'>Exp</div>
-    )
+      <>
+        {" "}
+        <div className="exp-grid">
+          <div className="exp-pic-div">
+            <div className="back">
+              {" "}
+              <div className="exp-30">
+                30<span>Years</span>
+                <span className="expr">Experience</span>
+              </div>{" "}
+            </div>
+            <div className="exp-pic">
+              <img src="./logo512.png " alt="" />
+            </div>
+          </div>
+          <div className="about">
+            <div className="about-div">
+              <span className="about-span">ABOUT US</span>
+              <h1>Unique Solutions For Residentials & Industries! </h1>
+            </div>
+            <div className="about-span  ">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+              vel, quibusdam asperiores eum possimus cum, reiciendis tempore,
+              vitae voluptates error soluta blanditiis excepturi laborum? Iusto
+              nulla suscipit cum quibusdam laborum?
+            </div>
+            <div className="about-serv">
+              <div>Ontime at services</div>
+              <div>24/7 hours services</div>
+              <div>Verified professionals</div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
   }
 }
 
-export default Exp
+export default Exp;
