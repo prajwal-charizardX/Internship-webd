1f756fb45613afaeb4d4a2de768f700eb3b665ff
2deadmen <kartikhegde.2002@gmail.com>
2022-10-03 11:41:40 +0530

    ksd

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -10,18 +10,15 @@ function HomePage(props) {
   return (
     <>
     <Header/>
-<<<<<<< HEAD
     <Cards setloader={props.setloader} />
      <Exp></Exp>
     <About></About>
     <Footer></Footer>
-=======
     <Slider/>
    
     <About/>
     {/* <Cards setloader={props.setloader} /> */}
     <Footer/>
->>>>>>> c3bc1b3107de18de657173033002c2cacde762f6
     </>
   )
 }
