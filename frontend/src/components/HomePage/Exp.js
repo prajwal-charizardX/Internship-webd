a6ebc986bbd4871fe058424203f73fba883fa5d7
2deadmen <kartikhegde.2002@gmail.com>
2022-10-04 18:25:14 +0530

    do

--- frontend/src/components/HomePage/Exp.js
+++ frontend/src/components/HomePage/Exp.js
@@ -1,23 +1,22 @@
-import React, { Component,useEffect } from "react";
+import React, { Component } from "react";
 import "./Exp.css";
 
-import AOS from 'aos';
-import 'aos/dist/aos.css';
 
-
-const Exp = () => {
- const cot= useEffect(() => {
-    AOS.init({ duration: 2000 });
-
-
-}, []);
-  return (
-    <>
-    {" "}
-    <div className="exp-grid" data-aos="slide-up">
-      <div className="exp-pic-div">
+export class Exp extends Component {
+  render() {
+    return (
+      <>
+        {" "}
+        <div className="exp-grid">
+          <div className="exp-pic-div">
+            
+              {" "}
+              <div className="exp-30">
+                <span className="n30">30</span><span>Years</span>
+                <span className="expr">Experience</span>
+              </div>{" "}
+            
         
-<<<<<<< HEAD
           </div>
           <div className="about">
             <div className="about-div">
@@ -33,37 +32,10 @@ const Exp = () => {
               <div className="about-serv-ch"> <i class="fa-sharp fa-solid fa-check"></i><div>Verified professionals</div></div>
             </div>
           </div>
-=======
-          {" "}
-          <div className="exp-30">
-            <span className="n30">30</span><span>Years</span>
-            <span className="expr">Experience</span>
-          </div>{" "}
-        
-    
-      </div>
-      <div className="about">
-        <div className="about-div">
-          <span className="about-span">ABOUT US</span>
-          <h1>Unique Solutions For Residentials & Industries! </h1>
->>>>>>> 1b3bbad35bd761626889c3bedb8ef09b2e58a54e
         </div>
-        <div className="about-span  ">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-          vel, quibusdam asperiores eum possimus cum, reiciendis tempore,
-          vitae voluptates error soluta blanditiis excepturi laborum? Iusto
-          nulla suscipit cum quibusdam laborum?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta voluptas aliquid natus incidunt, dolore ab modi consectetur dolores illum distinctio quibusdam dolorem praesentium pariatur alias quo asperiores earum quisquam laborum!
-        </div>
-        <div className="about-serv">
-          <div className="about-serv-ch"><i class="fa-sharp fa-solid fa-check"></i><div> Ontime at services</div></div>
-          <div className="about-serv-ch"> <i class="fa-sharp fa-solid fa-check"></i><div>24/7 hours services</div></div>
-          <div className="about-serv-ch"> <i class="fa-sharp fa-solid fa-check"></i><div>Verified professionals</div></div>
-        </div>
-      </div>
-    </div>
-  </>
-  )
+      </>
+    );
+  }
 }
 
-export default Exp
+export default Exp;
--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Cards from './Cards'
 
 import Slider from './Slider/Slider'
-
+import Header from './Header'
 import About from './About'
 import Exp from './Exp'
 
@@ -10,19 +10,16 @@ function HomePage(props) {
   
   return (
     <>
-<<<<<<< HEAD
-    <Header/>
+  
     {/* <Cards setloader={props.setloader} /> */}
      <Exp></Exp>
-    <About></About>
-  
-=======
- 
->>>>>>> 1b3bbad35bd761626889c3bedb8ef09b2e58a54e
-    <Slider/>
-     <Exp/>
 
-    <About/>
+     <About/>
+ 
+    <Slider/>
+   
+
+
     {/* <Cards setloader={props.setloader} /> */}
   
     
