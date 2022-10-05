b12cebde2d6bb5ffac9c4030b5d0867eca0840a6
2deadmen <kartikhegde.2002@gmail.com>
2022-10-05 19:18:29 +0530

    lkds

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -1,20 +1,17 @@
 import React from "react";
 import Cards from "./Cards";
 
-<<<<<<< HEAD
 import Slider from './Slider/Slider'
 
 import About from './About'
 import Exp from './Exp'
 import Leading from './leading-cons/Leading'
 import Special from './Special'
-=======
 import Slider from "./Slider/Slider";
 import { Scrollbars } from "react-custom-scrollbars-2";
 import About from "./About";
 import Exp from "./Exp";
 import Leading from "./leading-cons/Leading";
->>>>>>> 9a54d08dd9ef6108e37c9ee4eb630a9537d94640
 function HomePage(props) {
   return (
     <>
@@ -27,7 +24,6 @@ function HomePage(props) {
       <Cards setloader={props.setloader} />
 
      
-<<<<<<< HEAD
   
      <Exp/>
 
@@ -47,8 +43,6 @@ function HomePage(props) {
    
     
     
-=======
->>>>>>> 9a54d08dd9ef6108e37c9ee4eb630a9537d94640
     </>
   );
 }
