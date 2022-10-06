d72397f84e6812888f324ec73a74decd2401415b
akashgreninja <akashuhulekal@gmail.com>
2022-10-06 09:56:57 +0530

    fixed scrollbar and animation and something

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -1,46 +1,22 @@
 import React from "react";
 import Cards from "./Cards";
 
-import Slider from './Slider/Slider'
+import Slider from "./Slider/Slider";
 
-import About from './About'
-import Exp from './Exp'
-import Leading from './leading-cons/Leading'
-import Special from './Special'
-
-
+import About from "./About";
+import Exp from "./Exp";
+import Leading from "./leading-cons/Leading";
+import Special from "./Special";
 
 function HomePage(props) {
   return (
     <>
       <Slider />
-      <Leading />
-
       <Exp />
-
+      <Leading />
       <About />
       <Cards setloader={props.setloader} />
-
-     
-  
-     <Exp/>
-
-     <About/>
-     <Cards setloader={props.setloader} />
-     <Special/>
-     
-   
- 
-   
-   
-
-
-    {/* <Cards setloader={props.setloader} /> */}
-  
-    
-   
-    
-    
+      <Special />
     </>
   );
 }
